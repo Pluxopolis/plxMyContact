6ebc6aff19f8e2cb58bb96bcb1203bf7a6f717e1
Nicolas Danelon <nicolasdanelon@users.noreply.github.com>
2014-02-10 15:03:46 -0200

    adding spanish translate
    URL for test http://desarrollos-php.com.ar/index.php?contact

---
+++ lang/es.php
@@ -0,0 +1,46 @@
+<?php
+
+$LANG = array(
+
+'L_PAGE_TITLE'					=> 'Contacto',
+
+# config.php
+'L_MAIL_AVAILABLE'				=> 'Función Mail sending disponible',
+'L_MAIL_NOT_AVAILABLE'			=> 'Función Mail sending no disponible',
+
+'L_MENU_DISPLAY'				=> 'Mostrar en el menu superior la página de contacto',
+'L_MENU_TITLE'					=> 'Nombre en el menú',
+'L_MENU_TEXT'					=> 'Texto insertado en la parte superior del formulario',
+'L_MENU_POS'					=> 'Posición en el menú',
+'L_EMAIL'						=> 'Destinarios e-mail *',
+'L_EMAIL_CC'					=> 'Enviar una copia a los siguienste destinatarios (email) *',
+'L_EMAIL_BCC'					=> 'Enviar una copia oculta a los siguientes destinatarios (email) *',
+'L_EMAIL_SUBJECT'				=> 'Asunto del E-mail',
+'L_THANKYOU_MESSAGE'			=> 'Mensaje de gracias',
+'L_TEMPLATE'					=> 'Plantilla',
+'L_CAPTCHA'						=> 'Activar anti spam-capcha',
+'L_SAVE'						=> 'Guardar',
+'L_COMMA'						=> '* seperado por coma',
+
+'L_DEFAULT_MENU_NAME'			=> 'Contacto',
+'L_DEFAULT_OBJECT'				=> '## Pluxml ## nuevo contacto desde su website',
+'L_DEFAULT_THANKYOU'			=> 'Gracias por contactarse conmigo. Le responderé lo antes posible.',
+
+'L_ERROR_EMAIL'					=> 'Por favor ingrese un e-mail válido',
+
+# form.contact.php
+'L_ERR_NAME'					=> 'Por favor ingrese su nombre',
+'L_ERR_EMAIL'					=> 'Por favor ingrese un e-mail válido',
+'L_ERR_CONTENT'					=> 'Por favor ingrese un mensaje',
+'L_ERR_ANTISPAM'				=> 'Falló la verificación Anti-spam',
+'L_ERR_SENDMAIL'				=> 'Ocurrió un error al enviar el e-mail',
+
+'L_FORM_NAME'					=> 'Nombre',
+'L_FORM_MAIL'					=> 'Dirección de e-mail',
+'L_FORM_CONTENT'				=> 'Mensaje',
+'L_FORM_ANTISPAM'				=> 'Verificación Anti-spam',
+'L_FORM_BTN_SEND'				=> 'Enviar',
+'L_FORM_BTN_RESET'				=> 'Borrar todo',
+
+);
+?>

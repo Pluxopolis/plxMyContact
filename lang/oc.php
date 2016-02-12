82b8cfd0c01a0410529761da0250253439b14f4a
Stephane <stephane@pluxml.org>
2016-02-12 07:53:16 +0100

    Ajout traduction occitan (contriution Rubén)

---
+++ lang/oc.php
@@ -0,0 +1,47 @@
+<?php
+
+$LANG = array(
+
+'L_PAGE_TITLE'				=> 'Contacte',
+
+# config.php
+'L_MAIN'					=> 'General',
+'L_MAIL_AVAILABLE'			=> 'Fonccion de mandadís de corrièl disponible',
+'L_MAIL_NOT_AVAILABLE'		=> 'Fonccion de mandadís de corrièl indisponible',
+'L_MENU_DISPLAY'			=> 'Afichar lo menú de la pagina de contacte',
+'L_MENU_TITLE'		    	=> 'Títol del menú',
+'L_MENU_TEXT'		    	=> 'Tèxte a botar al dessús del formulari',
+'L_MENU_POS'		    	=> 'Posicion del menú',
+'L_EMAIL'		   			=> 'Destinatari(s) del corrièl *',
+'L_EMAIL_CC'	   			=> 'Destinatari(s) en copia del corrièl *',
+'L_EMAIL_BCC'	   			=> 'Destinatari(s) en copia amagada del corrièl *',
+'L_EMAIL_SUBJECT'  			=> 'Tòca del corrièl',
+'L_THANKYOU_MESSAGE'		=> 'Messatge de remerciament',
+'L_TEMPLATE'				=> 'Modèl',
+'L_CAPTCHA'					=> 'Activar lo captcha anti-indesirable',
+'L_URL'						=> 'Paramètre de l\'url',
+'L_SAVE'					=> 'Enregistrar',
+'L_COMMA'					=> '* separar las adreças de corrièl dels destinataris amb una virgula',
+
+'L_DEFAULT_MENU_NAME'		=> 'Contacte',
+'L_DEFAULT_OBJECT'			=> '## Pluxml ## nòu contacte dempuèi vòstre siti Internet',
+'L_DEFAULT_THANKYOU'		=> 'Mercés pel messatge, vos contactarem lèu !.',
+
+'L_ERROR_EMAIL'				=> 'Vos cal marcar una adreça valida se vos plai.',
+
+# form.contact.php
+'L_MSG_WELCOME'				=> 'Mercé de garnir lo formulari çai-sus',
+'L_ERR_NAME'				=> 'Mercé de picar vòstre nom',
+'L_ERR_EMAIL'				=> 'Mercé de picar una adreça de corrièl valida',
+'L_ERR_CONTENT'				=> 'Mercé de picar lo contengut de vòstre messatge',
+'L_ERR_ANTISPAM'			=> 'La verificacion anti-indesirable a fracassat',
+'L_ERR_SENDMAIL'			=> 'Una error s&rsquo;es produsida pendent la mandadís del vòstre messatge',
+
+'L_FORM_NAME'				=> 'Vòstre nom',
+'L_FORM_MAIL'				=> 'Adreça de corrièl',
+'L_FORM_CONTENT'			=> 'Contengut del messatge',
+'L_FORM_ANTISPAM'			=> 'Verificacion anti-indesirable',
+'L_FORM_BTN_SEND'			=> 'Mandar',
+'L_FORM_BTN_RESET'			=> 'Escafar',
+);
+?>

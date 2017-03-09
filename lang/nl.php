6ad9c1a6a9b151b54ebca0fea184bd3afcf67fd6
Stephane <Stphane@pluxml.org>
2017-03-09 11:25:38 +0100

    ajout traduc nl.php

---
+++ lang/nl.php
@@ -0,0 +1,52 @@
+<?php
+
+$LANG = array(
+
+'L_PAGE_TITLE'				=> 'Contact',
+
+# config.php
+'L_MAIN'				    => 'Hoofd',
+'L_MAIL_AVAILABLE'		    => 'Functie om mail te verzenden is beschikbaar',
+'L_MAIL_NOT_AVAILABLE'	    => 'Functie om mail te verzenden is niet beschikbaar',
+'L_MENU_DISPLAY'		    => 'Toon het menu van de contactpagina',
+'L_MENU_TITLE'		        => 'Menu titel',
+'L_MENU_TEXT'		        => 'Tekst ingevoegd boven aan het formulier',
+'L_MENU_POS'		        => 'Menu positie',
+'L_EMAIL'		   		    => 'E-mail ontvangers *',
+'L_EMAIL_CC'	   		    => 'CC-tje *',
+'L_EMAIL_BCC'	   		    => 'BCC-tje *',
+'L_EMAIL_SUBJECT'  		    => 'E-mail onderwerp',
+'L_APPEND_EMAIL_SUBJECT'    => 'Sta gebruiker toe om zijn eigen onderwerp hierboven toe te voegen ',
+'L_THANKYOU_MESSAGE'	    => 'Dank u bericht',
+'L_TEMPLATE'			    => 'Sjabloon',
+'L_CAPTCHA'			        => 'Activeer anti spam-capcha',
+'L_URL'				        => 'Url parameter',
+'L_SAVE'				    => 'Opslaan',
+'L_COMMA'				    => '* gescheiden door een komma',
+'L_LABEL'				    => 'Toon veld-label',
+'L_PLACEHOLDER'			    => 'Plaatsaanduiding voor het veld weergeven',
+
+'L_DEFAULT_MENU_NAME'	    => 'Contact',
+'L_DEFAULT_OBJECT'		    => '## Domaine Chaudeau ## nieuwe contactpersoon via uw website',
+'L_DEFAULT_THANKYOU'	    => 'Dank voor uw bericht. Ik zal zo spoedig mogelijk contact met u opnemen',
+
+'L_ERROR_EMAIL'			    => 'Vul alstublieft een geldig e-mail-adres in',
+
+# form.contact.php
+'L_MSG_WELCOME'				=> 'Bedankt voor het invullen van het formulier hieronder',
+'L_ERR_NAME'			    => 'Uw naam alstublieft',
+'L_ERR_EMAIL'			    => 'Vul alstublieft een geldig e-mail-adres in',
+'L_ERR_CONTENT'			    => 'Inhoud van uw bericht',
+'L_ERR_ANTISPAM'		    => 'Anti-spam verificatie is mislukt',
+'L_ERR_SENDMAIL'		    => 'Is een fout opgetreden tijdens het verzenden van uw bericht',
+
+'L_FORM_NAME'			    => 'Uw naam',
+'L_FORM_MAIL'			    => 'Uw e-mail adres',
+'L_FORM_SUBJECT'		    => 'Onderwerp van uw bericht',
+'L_FORM_CONTENT'		    => 'Inhoud van uw bericht',
+'L_FORM_ANTISPAM'		    => 'Anti-spam verificatie',
+'L_FORM_BTN_SEND'		    => 'Verzenden',
+'L_FORM_BTN_RESET'		    => 'Reset',
+
+);
+?>

d0e37798d03753a71e038910a605f5573d0989dc
Quentí <33203663+Quenty31@users.noreply.github.com>
2020-01-06 21:47:29 +0100

    Mise à jour occitan

--- lang/oc.php
+++ lang/oc.php
@@ -5,19 +5,19 @@ $LANG = array(
 'L_PAGE_TITLE'				=> 'Contacte',
 
 # config.php
-'L_LANG_UNAVAILABLE'		=> 'Langue non disponible : %s',
+'L_LANG_UNAVAILABLE'		=> 'Lenga non disponibla : %s',
 'L_MAIN'					=> 'General',
-'L_MAIL_AVAILABLE'			=> 'Fonccion de mandadís de corrièl disponible',
-'L_MAIL_NOT_AVAILABLE'		=> 'Fonccion de mandadís de corrièl indisponible',
+'L_MAIL_AVAILABLE'			=> 'Foncion de mandadís de corrièl disponibla',
+'L_MAIL_NOT_AVAILABLE'		=> 'Foncion de mandadís de corrièl indisponibla',
 'L_MENU_DISPLAY'			=> 'Afichar lo menú de la pagina de contacte',
 'L_MENU_TITLE'		    	=> 'Títol del menú',
 'L_MENU_TEXT'		    	=> 'Tèxte a botar al dessús del formulari',
 'L_MENU_POS'		    	=> 'Posicion del menú',
 'L_EMAIL'		   			=> 'Destinatari(s) del corrièl *',
-'L_EMAIL_CC'	   			=> 'Destinatari(s) en copia del corrièl *',
-'L_EMAIL_BCC'	   			=> 'Destinatari(s) en copia amagada del corrièl *',
+'L_EMAIL_CC'	   			=> 'Destinatari(s) en còpia del corrièl *',
+'L_EMAIL_BCC'	   			=> 'Destinatari(s) en còpia amagada del corrièl *',
 'L_EMAIL_SUBJECT'  			=> 'Tòca del corrièl',
-'L_APPEND_EMAIL_SUBJECT'    => 'Permettre à l\'utilisateur d\'ajouter son propre objet à celui ci-dessus',
+'L_APPEND_EMAIL_SUBJECT'    => 'Permetre a l\'utilizaire d\'ajustar son pròpri objècte a aqueste ça-jos',
 'L_THANKYOU_MESSAGE'		=> 'Messatge de remerciament',
 'L_TEMPLATE'				=> 'Modèl',
 'L_CAPTCHA'					=> 'Activar lo captcha anti-indesirable',
@@ -28,8 +28,8 @@ $LANG = array(
 'L_PLACEHOLDER'				=> 'Afichar los marcadors dins los camps del formulari de contacte (placeholder)',
 
 'L_DEFAULT_MENU_NAME'		=> 'Contacte',
-'L_DEFAULT_OBJECT'			=> '## Pluxml ## nòu contacte dempuèi vòstre siti Internet',
-'L_DEFAULT_THANKYOU'		=> 'Mercés pel messatge, vos contactarem lèu !.',
+'L_DEFAULT_OBJECT'			=> '## Pluxml ## nòu contacte de vòstre site Internet estant',
+'L_DEFAULT_THANKYOU'		=> 'Mercés pel messatge, vos contactarem lèu !',
 
 'L_ERROR_EMAIL'				=> 'Vos cal marcar una adreça valida se vos plai.',
 
